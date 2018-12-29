46bd1b2a35eb450e56ef19f2c1eb7ca48e13d335
agd <gauriaddepalli@gmail.com>
2018-12-29 13:56:01 -0500

    Update Merge2SortedLists.java

--- LinkedList/Merge2SortedLists.java
+++ LinkedList/Merge2SortedLists.java
@@ -10,12 +10,13 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         
         ListNode l3 = null;
-        ListNode l31 = null;
+        ListNode l31 = null; // the head  node
         
         if(l1 == null && l2 == null)return null;
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         
+        //placing the head for the new list
         if(l1.val<l2.val)
         {
            l3 = l1; 
@@ -25,9 +26,12 @@ class Solution {
            l3 = l2;
             l2=l2.next;
         }
+        
         l31 = l3;
+        
         while(l1 != null || l2 != null)
         {
+            // if any of the list reaches the end first.
             if(l1 == null) {l3.next = l2; break;}
             if(l2 == null) {l3.next = l1; break;}
             if(l1.val<l2.val)
