03f803870879576d8aaf5abd44efc83451f27014
agd <gauriaddepalli@gmail.com>
2019-01-10 22:24:42 -0500

    Create IntersectionofTwoArrays2.java

---
+++ Arrays/IntersectionofTwoArrays2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) 
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0; i<nums1.length; i++)
+        {
+            if(map.containsKey(nums1[i]))
+            {
+                map.put(nums1[i], map.get(nums1[i])+1);
+            }
+            else
+            {
+                map.put(nums1[i], 1);
+                
+            }
+        }
+        
+        for(int i=0; i<nums2.length; i++)
+        {
+            if(map.containsKey(nums2[i]))
+            {
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+                if(map.get(nums2[i]) >= 0)
+                {
+                    arr.add(nums2[i]);
+                }    
+            }
+        }
+        int[] a = new int[arr.size()];
+        for(int i=0; i<arr.size(); i++)
+        {
+            a[i] = arr.get(i);
+        }
+        
+        return a;     
+    }
+}
