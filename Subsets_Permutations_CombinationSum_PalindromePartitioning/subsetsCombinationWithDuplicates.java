b0b1d361de5b5a74d85f21c1dd4e1a3853bdbdbb
agd <gauriaddepalli@gmail.com>
2019-01-14 17:54:07 -0500

    Create subsetsCombinationWithDuplicates.java

---
+++ Subsets_Permutations_CombinationSum_PalindromePartitioning/subsetsCombinationWithDuplicates.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> allSubsetList = new ArrayList<List<Integer>>();
+        
+        //sort the numbers
+        Arrays.sort(nums);
+        
+        // generate all the P & C.
+       recursivebt(allSubsetList, new ArrayList<Integer>(), nums, 0);
+        
+        return allSubsetList;   
+    }
+    
+    // There will in all be 2 ^ n subsets.
+    
+    public void recursivebt(List<List<Integer>> allSubsetList, List<Integer> temp, int [] nums, int start)
+    {
+        // add the subsets in the list while entering the the recurive code, so that empty list is added too.
+        allSubsetList.add(new ArrayList<Integer>(temp));
+        // iterating through the array.
+        for(int i =start; i<nums.length; i++)
+        {
+         // the condition to exclude duplicates.
+            if(i>start && nums[i]==nums[i-1]) continue;
+            
+        // keeping the first array in the list constant.
+         temp.add(nums[i]);
+            
+         // call the recursive code for the next of the following values.
+         recursivebt(allSubsetList, temp, nums, i+1);
+            
+         // remove the element latest added to add other options.
+         temp.remove(temp.size()- 1);
+        }
+        
+    }
+}
