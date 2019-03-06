fc3dcaf546e362bf7f1e44f61c99612e9328bda9
agd <gauriaddepalli@gmail.com>
2019-03-06 00:51:45 -0500

    Create topKFrequent.java

---
+++ Heap/topKFrequent.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        
+        for(String w: words){
+            if(hm.containsKey(w)){
+                hm.put(w, hm.get(w)+1);
+            }else{
+                hm.put(w,1);
+            }   
+        }
+        // sorts in ascending order
+        PriorityQueue<String> heap = new PriorityQueue<String>((w1,w2) -> hm.get(w1).equals(hm.get(w2)) ? w2.compareTo(w1) : hm.get(w1)-hm.get(w2));
+       
+        for(String w: hm.keySet()) {
+            heap.offer(w);
+            if(heap.size()>k){
+                heap.poll();
+            }
+        }
+        
+        List<String> ans = new ArrayList();
+        while( !heap.isEmpty()){
+            ans.add(heap.poll());
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
