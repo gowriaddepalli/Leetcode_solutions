59789f9f3c172ba262a28b8f6baf3f3d4735a6e5
agd <gauriaddepalli@gmail.com>
2019-07-31 11:58:18 -0700

    Create kth-smallest-element-in-a-sorted-matrix.java

---
+++ FrequentQuestions/kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,37 @@
+Solution 1 : Heap
+Here is the step of my solution:
+
+Build a minHeap of elements from the first row.
+Do the following operations k-1 times :
+Every time when you poll out the root(Top Element in Heap), you need to know the row number and column number of that element(so we can create a tuple class here), replace that root with the next element from the same column.
+
+
+
+
+ublic class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
+        for(int j = 0; j <= n-1; j++) pq.offer(new Tuple(0, j, matrix[0][j]));
+        for(int i = 0; i < k-1; i++) {
+            Tuple t = pq.poll();
+            if(t.x == n-1) continue;
+            pq.offer(new Tuple(t.x+1, t.y, matrix[t.x+1][t.y]));
+        }
+        return pq.poll().val;
+    }
+}
+
+class Tuple implements Comparable<Tuple> {
+    int x, y, val;
+    public Tuple (int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+    
+    @Override
+    public int compareTo (Tuple that) {
+        return this.val - that.val;
+    }
+}
