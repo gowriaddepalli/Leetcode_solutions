09b630f791a00c6b82706ca2013704d332addc16
agd <gauriaddepalli@gmail.com>
2020-01-04 01:50:31 -0500

    Create maxDistToClosest.java

---
+++ Arrays/maxDistToClosest.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int prev = -1;
+        int future =0;
+        int N = seats.length;
+        int ans =0;
+        
+        for(int i=0; i<N; i++){
+            if(seats[i] == 1){
+                prev =i;
+            }else{
+                while(future<N && seats[future] ==0 || (future<i)){
+                    future++;
+                }
+                int left = prev == -1 ? N : i-prev;
+                int right = future == N ? N : future-i;
+            
+                ans = Math.max(ans, Math.min(left,right));
+            }
+            
+            
+            
+        }
+        
+        return ans;
+    }
+}
