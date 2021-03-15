3a3364faac60b5b6924ee0728e07c6eef9f06b5d
Gowri Addepalli <gauriaddepalli@gmail.com>
2021-03-14 20:45:48 -0400

    Time: 0 ms (100.00%), Space: 36.7 MB (71.94%) - LeetHub

--- find-the-highest-altitude/find-the-highest-altitude.java
+++ find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,6 +1,6 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         int n = gain.length;
         int alt = 0;
         
@@ -10,10 +10,8 @@ class Solution {
                 max = alt;
             }
         }
-        System.out.println(max);
         
-        //return ((max>gain[0]) ? (max>0)? max :0 :(gain[0]>0)? gain[0] :0);
-        return (max>0? max: 0);
+        return max;
         
     }
 }
