55bf662e42ad81f636de763bc9e93ec0acef2422
Gowri Addepalli <gauriaddepalli@gmail.com>
2021-03-16 18:51:53 -0400

    Time: 36 ms (68.91%), Space: 14.3 MB (11.29%) - LeetHub

--- height-checker/height-checker.py
+++ height-checker/height-checker.py
@@ -1,4 +1,3 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        return sum(a != b for a,b in zip(heights, sorted(heights)))
-        
+        return len(heights) - sum([(x == y) for x,y in zip(heights, sorted(heights))])
