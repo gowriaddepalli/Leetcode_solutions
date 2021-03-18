544a951c2847b5ae926c5739d0f191940247cb50
Gowri Addepalli <sga297@nyu.edu>
2021-03-18 10:06:39 -0400

    Update find-lucky-integer-in-an-array.py

--- find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -18,3 +18,21 @@ class Solution:
         else:
             return max(lucky_items)
         
+        
+        
+        
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+    # We can simply pass the input array into the Counter constructor,
+    # and it will convert it into a dictionary of num -> count.
+        counts = collections.Counter(arr)
+        largest_lucky_number = -1
+        # Check each num -> count pair in the dictionary.
+        for num, count in counts.items():
+            # If this is a lucky number
+            if num == count:
+                # Keep the largest out of this lucky number and our current largest.
+                largest_lucky_number = max(largest_lucky_number, num)
+        return largest_lucky_number
+        
+        
