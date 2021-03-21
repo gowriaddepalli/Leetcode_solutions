78ef9586bb55e979b573d22cc9996fc9359dff9c
Gowri Addepalli <gauriaddepalli@gmail.com>
2021-03-21 16:50:22 -0400

    Time: 88 ms (48.30%), Space: 15.4 MB (82.68%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,19 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:  
-        result = self.binary_search(nums,0,len(nums)-1, target)
-        print(result)
+    def searchRange(self, nums: List[int], target: int) -> List[int]: 
+        
+        def binary_search(nums: List[int], low: int, high:int, target: int) -> int:
+            if(high >= low):
+                mid = low + (high - low) //2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    return binary_search(nums,low,mid-1,target)
+                else:
+                    return binary_search(nums,mid+1,high,target)
+            else:
+                return -1
+            
+        result = binary_search(nums,0,len(nums)-1, target)
         if(result == -1):
             return [-1,-1]
         real_low = result
@@ -10,20 +22,8 @@ class Solution:
             real_low =real_low-1
         while(real_high+1<len(nums) and nums[real_high+1] == target):
             real_high =real_high+1
-        #print(real_low)
-        #print(real_high)
         return [real_low,real_high]
     
-    def binary_search(self, nums: List[int], low: int, high:int, target: int) -> int:
-            if(high >= low):
-                mid = (high+low)//2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    return self.binary_search(nums,low,mid-1,target)
-                else:
-                    return self.binary_search(nums,mid+1,high,target)
-            else:
-                return -1
+    
             
         
