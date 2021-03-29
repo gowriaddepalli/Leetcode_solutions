175894ae3e921e3570886f99680140f21acaf242
Gowri Addepalli <gauriaddepalli@gmail.com>
2021-03-29 13:15:17 -0400

    Time: 61 ms (29.53%), Space: 47.7 MB (35.63%) - LeetHub

--- design-browser-history/design-browser-history.java
+++ design-browser-history/design-browser-history.java
@@ -6,31 +6,33 @@ class BrowserHistory {
     public BrowserHistory(String homepage) {
         forward = new Stack<String>();
         backward = new Stack<String>();
+        // Initialize the stack with homepage.
         backward.push(homepage);
         
     }
     
     public void visit(String url) {
         backward.push(url);
+        //Whenever you visit any page, the forward goes empty or reinitialize.
         forward = new Stack<String>();
         
     }
     
     public String back(int steps) {
-        String popped = "";
+        // Always ensure 1 element is always there in the stack
         while(backward.size() >1 && steps >0){
-            popped= backward.pop();
-            forward.push(popped);
+            forward.push(backward.peek());
+            backward.pop();
             steps--;
         }
         return backward.peek();
     }
     
     public String forward(int steps) {
-        String popped = "";
+        // Add the items from forward to backward stack.
         while(forward.size()>0 && steps >0){
-            popped = forward.pop();
-            backward.push(popped);
+            backward.push(forward.peek());
+            forward.pop();
             steps--;
         }
         return backward.peek();       
