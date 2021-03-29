9bd14b7bbc2c46e0c49be0a26cd8e7f31b302184
Gowri Addepalli <sga297@nyu.edu>
2021-03-29 02:26:24 -0400

    Update ParkingSystem.py

--- Leetcode_design/ParkingSystem.py
+++ Leetcode_design/ParkingSystem.py
@@ -2,17 +2,12 @@
 ##
 class ParkingSystem:
 
-    def __init__(self, big: int, medium: int, small: int):
-        self.car_space = {1:big,2:medium,3:small}
-        
-        
+    def __init__(self, big, medium, small):
+        self.A = [big, medium, small]
 
-    def addCar(self, carType: int) -> bool:
-        if self.car_space[carType]<1:
-            return False
-        else:
-            self.car_space[carType] = self.car_space[carType]-1
-            return True
+    def addCar(self, carType):
+        self.A[carType - 1] -= 1
+        return self.A[carType - 1] >= 0
         
         
 
